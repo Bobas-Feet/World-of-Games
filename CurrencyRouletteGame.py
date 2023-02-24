ab16109d8de240a891a712777137a937a8732527
Bobas-Feet <115990871+Bobas-Feet@users.noreply.github.com>
2023-02-24 16:30:28 +0200

    Update CurrencyRouletteGame.py
    Restoring fuck-ups 
    24/2/2023

--- CurrencyRouletteGame.py
+++ CurrencyRouletteGame.py
@@ -1,35 +1,42 @@
-import requests
+# 24/2/2023
+
+import json
+import os
+import urllib.request as r
 import random
 from credentials import api_key
 
 
-def get_money_interval():
-    rng = random.randint(1, 100)
-    curr_from = input('Please enter the currency from which you would like to convert: ').upper()
-    curr_to = input('Please enter the currency to which you would like to convert: ').upper()
-    amount = int(input('Please enter the amount you would like to convert: '))
-    return curr_from, curr_to, amount
+def get_money_interval(difficulty):
 
-    # Get converted amount
+    url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/USD/ILS'
+    response = r.urlopen(url)
+    data = json.load(response)
+
+    ex = int(data["USD_ILS"])
+    rng = int(random.uniform(1, 100))
+    t = rng * ex
+    low = int(t - (5 - difficulty))
+    high = int(t + (5 - difficulty))
+    return rng, t, low, high
 
 
-def get_converted_amount(curr_from, curr_to, amount):
-    #
-    # url = f'https://v6.exchangerate-api.com/v6/{api_key}/pair/{curr_from}/{curr_to}/{amount}'
-    #     # Parse the result as JSON
-    # data = requests.get(url).json()
-    #     # Extract the converted amount
-    # converted_amount = data['conversion_result']
-
-    # return converted_amount
+def get_guess_from_user(rng):
+    while True:
+        try:
+            guess = int(input(f"Guess the value of ${rng} in ILS: "))
+        except ValueError:
+            print("Error: You can only enter numbers.")
+            continue
+        return guess
     return
 
-def play():
-    if __name__ == '__main__':
-        curr_from, curr_to, amount = get_money_interval()
-        converted_amount = get_converted_amount(curr_from, curr_to, amount)
-        print(f'{amount} {curr_from} = {converted_amount} {curr_to}')
-    get_converted_amount(curr_from=0, curr_to=0, amount=0)
-    get_money_interval()
 
-
+def play(difficulty):
+    rng, t, low, high = get_money_interval(difficulty)
+    guess = get_guess_from_user(rng)
+    os.system('cls' if os.name == 'nt' else 'clear')
+    if high >= guess >= low:
+        return True
+    else:
+        return False
